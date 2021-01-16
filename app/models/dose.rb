d522cf2c0f5294a4b6a74de970b08b86270de5c7
Igor Stein <igor_stein@yahoo.de>
2021-01-16 12:24:44 +0100

    trying to commit everything

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,8 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+
+  validates :description, :cocktail, :ingredient, presence: true
 end
+
+# [cocktail, ingredient] pairings should be unique.
